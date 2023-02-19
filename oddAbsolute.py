dceac269a1ed0bc35cfb267bfb66fc78de2230aa
Kyle Kroll <Kyle.Kroll@vu.com>
2023-02-19 16:33:29 -0600

    Completed function to complete assignment

--- oddAbsolute.py
+++ oddAbsolute.py
@@ -1,7 +1,16 @@
 def calculateAbsolute():
     
     # This first line is provided for you
-    in_num  = input("Enter a number: ")
+        try:
+            in_num  = input("Enter a number: ")
+            n = int(in_num)
+            if n > 21:
+                x = n - 21
+                print("Result:", 2 * x)
+            else:
+                print("Result:", 21 - n)
+        except:
+            print("Please enter numerical digit")
     # end assignment
 
 ## If you want to test locally run > python payCalculator.py
